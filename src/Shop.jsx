71d696e3b5d22214bfbfc1d40104c9f9911f9742
Saul <129878391+weafrica@users.noreply.github.com>
2026-07-27 13:31:37 +0200

    Add files via upload

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { supabase } from "./supabaseClient";
 import {
   ArrowLeft, X, Plus, Minus, Trash2, Upload, CheckCircle2, XCircle, Clock,
